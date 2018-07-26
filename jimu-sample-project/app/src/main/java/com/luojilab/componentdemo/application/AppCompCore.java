33b1c84aa2ecbbcc1455b2b0ccf46913945023af
mac <lulu@macdeMacBook-Pro.local>
2018-07-26 19:30:52 +0800

    提交运行demo
    主要提交利用asm的方式动态添加组件的注册功能

---
+++ jimu-sample-project/app/src/main/java/com/luojilab/componentdemo/application/AppCompCore.java
@@ -0,0 +1,20 @@
+package com.luojilab.componentdemo.application;
+
+import android.util.Log;
+
+import com.luojilab.component.componentlib.applicationlike.IApplicationLike;
+
+public class AppCompCore {
+
+
+    public static void rigisterComp(IApplicationLike iApplicationLike){
+        Log.d("asm---",""+iApplicationLike.getClass().getSimpleName());
+        iApplicationLike.onCreate();
+    }
+
+    public static void initComp(){
+
+    }
+
+
+}
